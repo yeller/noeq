6fe73aff5970eb590a95426ae72ec56c37710622
Tom Crayford <tcrayford@googlemail.com>
2014-03-30 13:23:07 +0100

    fix up project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject noeq "0.1.0-SNAPSHOT"
   :global-vars {*warn-on-reflection* true}
   :java-source-paths ["java"]
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure client for noeqd"
+  :url "https://github.com/tcrayford/noeq"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
